ee7e1ba520ff22fb9947ad0312eb5b7a7ff59ff6
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-01-25 08:12:30 -0500

    Add LoadingBar component

---
+++ src/components/LoadingBar.js
@@ -0,0 +1,23 @@
+import { CircularProgress } from '@mui/material';
+import React from 'react';
+
+/**
+ * The LoadingBar displays a circular progress bar while blurring
+ * the background as content loads into the screen.
+ * @returns {JSX.Element}
+ * @see https://mui.com/components/progress/
+ */
+const LoadingBar = () => {
+
+  return <>
+        <CircularProgress/>
+  </>;
+  
+};
+
+/**
+ * The LoadingBar displays a circular progress bar while blurring
+ * the background as content loads into the screen.
+ * @returns {JSX.Element}
+ */
+export default LoadingBar;
