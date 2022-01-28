bbd696c430c4cc7a181ce52de3e7642430ac803b
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-01-28 01:02:53 -0500

    Adjustments: Responsive Navigation Panel
    The navigation panel has been made more responsive - transitions to an "overlay" style hamburger menu when the screen width is less than 800 px

--- src/components/NavigationPanel.js
+++ src/components/NavigationPanel.js
@@ -16,12 +16,30 @@ const NavigationPanel = (props) => {
 
     /**
      * Stylesheet object that stores the state of the showNav
-     * prop.
+     * prop based on whether the user viewport is greater or
+     * equal to 800px.
      */
-    const visibility = {display: props.showNav ? 'block' : 'none'}
+    const visibility = {
+        'false': {
+            transform: props.showNav ? 'translateX(0)' : 'translateX(-70vw)'
+        },
+        'true': {
+            display: props.showNav ? 'block' : 'none'
+        }
+    }
+
+    /**
+     * Toggles the navigation panel.
+     */
+    const toggleNavigationPanel = () => {
+
+        if (window.innerWidth >= 800) {return}
+        props.navToggle()
+    }
 
     return (
-        <div className='navigation__container' style={visibility}>
+        <div className='navigation__container' style={{...visibility[window.innerWidth >= 800]}}>
+            <i id="close__btn" className='fas fa-close' onClick={toggleNavigationPanel}></i>
             <div className='branding'>
                 <i class="fas fa-paper-plane"></i>
                 <div className='branding__logo'>
@@ -30,9 +48,9 @@ const NavigationPanel = (props) => {
                 </div>
             </div>
             <ul className="nav__menu">
-                <Link to="/"><li className={loc.pathname === '/' ? 'nav__item active' : 'nav__item'}>Dashboard</li></Link>
-                <Link to="/applications"><li className={loc.pathname === '/applications' ? 'nav__item active' : 'nav__item'}>Job Applications</li></Link>
-                <Link to="/settings"><li className={loc.pathname === '/settings' ? 'nav__item active' : 'nav__item'}>Settings</li></Link>
+                <Link onClick={toggleNavigationPanel} to="/"><li className={loc.pathname === '/' ? 'nav__item active' : 'nav__item'}>Dashboard</li></Link>
+                <Link onClick={toggleNavigationPanel} to="/applications"><li className={loc.pathname === '/applications' ? 'nav__item active' : 'nav__item'}>Job Applications</li></Link>
+                <Link onClick={toggleNavigationPanel} to="/settings"><li className={loc.pathname === '/settings' ? 'nav__item active' : 'nav__item'}>Settings</li></Link>
             </ul>
         </div>
     )
